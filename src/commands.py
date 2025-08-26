fd59e8b04a8c3de67ea38167188d54bf986e063f
GL1KK <danya_serov05@internet.ru>
2025-08-26 15:26:57 +0300

    add save in file

---
+++ src/commands.py
@@ -0,0 +1,6 @@
+import keyboard 
+
+class Commands:
+
+    def hanle_up(self, line: int, buffer: list):
+        pass
--- src/editor.py
+++ src/editor.py
@@ -8,10 +8,20 @@ class TextEditor():
     def edit_text(self):
         ext = self.filename.split(".")[-1] 
         self.utils.design(extension=ext)
-        num = 1
-        while True:
-            txt = input(f"{num} ")
-            num += 1
-            self.buffer.append(txt)
-
-            
+        num_line = 1
+        with open(self.filename, "a+") as file:
+            file.seek(0)
+            content = file.readlines()
+            for i in content:
+                print(f"{num_line} {i}")
+                num_line += 1
+            while True:
+                txt = input(f"{num_line} ")
+                num_line += 1
+                self.buffer.append(txt)
+                if txt == "all":
+                    break 
+            for i in range(len(self.buffer)-1):
+                file.write(f"{self.buffer[i]}\n")
+            else:
+                self.buffer.clear()
